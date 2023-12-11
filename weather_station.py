a1dfaa6c904c53a93df32d188c56c075d41a181f
Tomasz Drozdowski <tomasz.drozdowski@op.pl>
2023-12-11 17:01:30 +0100

    Implemented additional current weather info

--- weather_station.py
+++ weather_station.py
@@ -30,6 +30,7 @@ def main():
     curr_temp_small_font = ImageFont.truetype("arialbd.ttf", 32)
     date_font = ImageFont.truetype("arial.ttf", 14)
     font = ImageFont.truetype("arial.ttf", 24)
+    add_info_font = ImageFont.truetype("arial.ttf", 16)
 
     # Getting weather from API
     ow = wlib.OpenWeather(API_KEY, 'metric', 'pl', LATITUDE, LONGITUDE)
@@ -39,8 +40,12 @@ def main():
     main_image = Image.new('1', (SCREEN_WIDTH, SCREEN_HEIGHT), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(main_image)
 
-    # Date
+
+    ### CURRENT DATE ###
     draw.text((10, 7), datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S"), font=date_font)
+
+
+    ### CURRENT WEATHER MAIN INFO ###
 
     # Background - black
     draw.rounded_rectangle((15, 27, 270+15, 190+27), radius=15, fill=0)
@@ -63,6 +68,45 @@ def main():
     icon_image = Image.open(weather.current.weather.get_icon_image(save=True)).resize((80,) * 2)
     icon_image = transparency_to_white(icon_image)  # use when resized and white background (resizing changes transparency to black)
     main_image.paste(icon_image, box=(190, 33))
+
+
+    ### ADDITIONAL CURRENT INFO ###
+
+    ADD_INFO_Y_POS = 125
+    ADD_INFO_Y_SPACE = 30
+    ADD_INFO_X_POS = 35
+    ADD_INFO_X_SPACE = 150
+
+    # Precipitations values
+    rain = weather.current.rain
+    rain = rain.get('1h', 0) if rain is not None else 0
+    snow = weather.current.snow
+    snow = snow.get('1h', 0) if snow is not None else 0
+
+    info_element = [
+        [
+            {'icon': "./images/precip.png", 'value': rain + snow, 'unit': " mm/h"},
+            {'icon': "./images/cloud.png", 'value': weather.current.clouds, 'unit': " %"},
+            {'icon': "./images/pressure.png", 'value': weather.current.pressure, 'unit': " hPa"},
+        ],
+        [
+            {'icon': "./images/wind.png", 'value': weather.current.wind_speed, 'unit': " m/s"},
+            {'icon': "./images/humidity.png", 'value': weather.current.humidity, 'unit': " %"},
+        ]
+    ]
+    for col_no, column in enumerate(info_element):
+        for row_no, element in enumerate(column):
+            # Icon
+            try:
+                icon_image = Image.open(element['icon'])
+                main_image.paste(icon_image, box=(ADD_INFO_X_POS + ADD_INFO_X_SPACE * col_no, ADD_INFO_Y_POS + ADD_INFO_Y_SPACE * row_no))
+            except FileNotFoundError as exc:
+                print(exc)
+
+            # Value, unit
+            val = round(element['value'], 1)
+            draw.text((ADD_INFO_X_POS + 25 + ADD_INFO_X_SPACE * col_no, ADD_INFO_Y_POS + ADD_INFO_Y_SPACE * row_no), 
+                      str(val) + element['unit'], font=add_info_font, fill=1)
 
 
     # Image save and show
