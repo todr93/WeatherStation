a3df0748dcdc7cf8e5da8af49e96cbed5566507c
Tomasz Drozdowski <tomasz.drozdowski@op.pl>
2024-01-09 20:31:22 +0100

    Added page with photos

--- web_app.py
+++ web_app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import send_from_directory
+import os
 import weather_station
 
 app = Flask('weather')
@@ -16,6 +18,22 @@ def settings_view():
         weather_station.main('screen')
 
     return render_template('settings.html')
+
+@app.route('/photos')
+def photos_view():
+    # Find all files in photos dir
+    dir = os.path.join(os.path.curdir, 'photos')
+    photos = [filename for filename in os.listdir(dir)]
+
+    data = {
+        'photos': photos,
+    }
+
+    return render_template('photos.html', data=data)
+
+@app.route('/photos/<path:filename>')
+def send_photo(filename):
+    return send_from_directory('photos', filename, as_attachment=True)
 
 
 if __name__ == '__main__':
