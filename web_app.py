f5c215d058440d553005e425e8df340c0db252a6
Tomasz Drozdowski <tomasz.drozdowski@op.pl>
2024-05-03 10:30:54 +0200

    Implement removing images option

--- web_app.py
+++ web_app.py
@@ -23,6 +23,10 @@ class NewPhotoForm(FlaskForm):
             raise ValidationError('Dozwolone typy plików to: jpg, bmp, tiff i png')
         else:
             return True
+        
+
+class RemovePhotoForm(FlaskForm):
+    file_name = StringField('Nazwa pliku do usuniecia')
 
 
 @app.route('/')
@@ -40,10 +44,12 @@ def settings_view():
 @app.route('/photos', methods=['GET', 'POST'])
 def photos_view():
     new_photo_form = NewPhotoForm()
+    remove_photo_form = RemovePhotoForm()
+
     photo_dir = os.path.join(os.path.curdir, 'photos')
 
     if request.method == 'POST':
-        if new_photo_form.validate_on_submit() == True:
+        if 'new-photo-form' in request.form and new_photo_form.validate_on_submit():
             file = new_photo_form.data['file']
             img = Image.open(file)
             new_height = 240
@@ -51,12 +57,19 @@ def photos_view():
             img = img.resize((new_width, new_height))
             img.save(os.path.join(photo_dir, file.filename))
 
+        if 'remove-photo-form' in request.form:
+            file_name = remove_photo_form.data['file_name']
+            file_path = os.path.join(photo_dir, file_name)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
     # Find all files in photos dir
     photos = [filename for filename in os.listdir(photo_dir)]
 
     data = {
         'photos': photos,
         'new_photo_form': new_photo_form,
+        'remove_photo_form': remove_photo_form,
     }
 
     return render_template('photos.html', data=data)
